91dae7e80b963575690f0d6b3d550f39626eaa8c
Kyle Travers <traverskyle9@gmail.com>
2023-02-12 21:44:12 -0600

    Fixed git prompt.
    Bash takes a couple of seconds to start, like in Windows.
    IDK its working now.

--- Git/git-prompt.linux.sh
+++ Git/git-prompt.linux.sh
@@ -18,15 +18,15 @@ PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
 PS1="$PS1"'\w'                 # current working directory
 
 GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
-COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
-COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
-COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
-if test -f "$COMPLETION_PATH/git-prompt.sh"
+COMPLETION_PATH="$GIT_EXEC_PATH"
+if test -f "$COMPLETION_PATH/git-sh-prompt"
 then
-	. "$COMPLETION_PATH/git-completion.bash"
-	. "$COMPLETION_PATH/git-prompt.sh"
+	#. "$COMPLETION_PATH/git-completion.bash"
+	. "$COMPLETION_PATH/git-sh-prompt"
 	PS1="$PS1"'\[\033[36m\]'  # change color to cyan
 	PS1="$PS1"'`__git_ps1`'   # bash function
+else
+	echo "Failed to setup prompt..."
 fi
 
 PS1="$PS1"'\[\033[0m\]'        # change color
